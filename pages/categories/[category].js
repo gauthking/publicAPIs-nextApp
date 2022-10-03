2d8ec3aa3f287d8fa06854b519cb6956862fbf8f
Gautham Krishna <90638995+gauthking@users.noreply.github.com>
2022-10-03 18:36:42 +0530

    fixed minor error in darkmodetext

--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -89,7 +89,7 @@ function Index({ data, categories }) {
                     <label className="inline-flex relative items-center cursor-pointer">
                         <input onClick={() => toggleModes()} type="checkbox" className="sr-only peer" />
                         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
-                        <span className={`ml-3 text-sm font-medium text-black`}>Dark Mode</span>
+                        <span className={`ml-3 text-sm font-medium ${toggle === true ? "text-white" : "text-black"}`}>{toggle === true ? "Light Mode" : "Dark Mode"}</span>
                     </label>
                 </div>
             </div >
